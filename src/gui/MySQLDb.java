bb49dcf75c6ca41d515ed181f3ff18f9660d874d
Bourhane Younes <bourhaneyounes1@gmail.com>
2022-12-18 18:30:11 +0100

    fixed sql query statement

--- src/gui/MySQLDb.java
+++ src/gui/MySQLDb.java
@@ -7,15 +7,17 @@ import java.util.List;
 
 public class MySQLDb {
 
-	private Connection conn = null;
+	private Connection conn;
 	private Statement statement = null;
+	private PreparedStatement prepstat = null;
 	private ResultSet resultSet = null;
 	
-	public Connection getConn() {
+	
+	private void getConn() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			
-			conn = DriverManager.getConnection(  
+			this.conn = DriverManager.getConnection(  
 					"jdbc:mysql://localhost:3306/java_mini_projet","root","calimero");
 			
 			//conn = DriverManager.getConnection(
@@ -28,40 +30,65 @@ public class MySQLDb {
 				System.out.println(e);
 		}
 		
-		return conn;
+	}
+	
+	public void insertDb(String[] data) throws SQLException {
+		getConn();
+		String query = "INSERT INTO Students (cne, nom, prenom, email, filiere, tel, ville)"
+				+ "VALUES (?, ?, ?, ?, ?, ?, ?)";
+		
+		try {
+	
+			prepstat = this.conn.prepareStatement(query);
+
+			for (int i = 0; i < data.length; i++)
+				if(data[i] != null)
+					prepstat.setString(i+1, data[i]);
+				else
+					prepstat.setString(i+1, "null");
+			
+			prepstat.execute();
+			this.conn.close();
+			
+		} catch (Exception e) {
+			System.err.println("Exception");
+			e.printStackTrace();
+			System.out.println(e);
+		}
+		
 	}
 	public String[][] readDb(String att) throws Exception{
-		
+		getConn();
 		List<List<String>> data = new ArrayList<List<String>>();
 		
-		conn = getConn();
-			
-			
-		statement = conn.createStatement();
+		statement = this.conn.createStatement();
 		
 		if (att.isBlank())
 			resultSet = statement.executeQuery("SELECT * FROM Students");
 		else
-			resultSet = statement.executeQuery("SELECT * FROM Students WHERE name = " + att);
+			resultSet = statement.executeQuery("SELECT * FROM Students WHERE nom = '" + att + "'");
 			
 		int i = 0;
 		while(resultSet.next()) {
 			data.add(new ArrayList<String>());
 			for (int j = 0; j < 7; j++) {
+				//System.out.println(resultSet.getString(j+1));
 				data.get(i).add(resultSet.getString(j+1));
 			}
 			i++;
 		}
 			
-	conn.close();
+	this.conn.close();
 		
 	String[][] dataFormated = new String[data.size()][7];
 	
-	for (int c = 0; i < data.size(); c++) {
+	for (int c = 0; c < data.size(); c++) {
 		for (int j = 0; j < 7; j++) {
 			dataFormated[c][j] = data.get(c).get(j);
 		}
 	}
+	
+//	System.out.println(dataFormated[0][0]);
 	
 	return dataFormated;	
 	}
